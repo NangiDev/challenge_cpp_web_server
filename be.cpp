2773573677db5190ea9aabdd22f2b3e114b43a2c
Joel Setterberg <j.settrbrg@gmail.com>
2024-04-20 14:17:27 +0200

    continous running server

--- be.cpp
+++ be.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
-using namespace boost::asio;
-using ip::tcp;
+using boost::asio::ip::tcp;
 
 std::string read_(tcp::socket & socket) {
 	boost::asio::streambuf buf;
@@ -21,13 +20,27 @@ int main(int args, char* argv[])
 	std::cout << "Server is running!" << std::endl;
 
 	boost::asio::io_service io_service;
-	tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 8080 ));
-	tcp::socket socket_(io_service);
-	acceptor_.accept(socket_);
-	std::string message = read_(socket_);
-	std::cout << message << std::endl;
-	send_(socket_, "Hello From Server!");
+	tcp::endpoint endpoint(tcp::v4(), 8080);
+	tcp::acceptor acceptor_(io_service, endpoint);
 
-	std::cout << "Server sent Hello message to Client!" << std::endl;
+	while(true) {
+		tcp::socket socket_(io_service);
+		acceptor_.accept(socket_);
+
+		std::string message = read_(socket_);
+		std::cout << "Received message: " << message << std::endl;
+
+		if (message.find("/hello") != std::string::npos) {
+			send_(socket_, "Hello from Server!");
+		} else if (message.find("/bye") != std::string::npos) {
+			send_(socket_, "Goodbye from Server!");
+			break;
+		} else {
+			send_(socket_, "Invalid path!");
+		}
+
+		std::cout << "Server sent Hello message to Client!" << std::endl;
+	}
+		
 	return 0;
 }
